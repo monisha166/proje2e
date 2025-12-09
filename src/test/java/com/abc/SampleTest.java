c0d2dd7e9246e7f5f09f42da6e3c63f08f406c6e
monisha166 <monishamegha@gmail.com>
2025-12-08 23:14:34 -0800

    Initial commit - proje2e maven web application

---
+++ src/test/java/com/abc/SampleTest.java
@@ -0,0 +1,15 @@
+package com.abc;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+        int a = 2;
+        int b = 3;
+        assertEquals(5, a + b);
+    }
+}
+
